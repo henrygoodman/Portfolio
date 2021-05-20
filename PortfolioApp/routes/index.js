d340535368ae8233e38fe4830237cef34715359e
Henry Goodman <13032204@student.uts.edu.au>
2021-05-20 19:53:02 +1000

    Add all basic views

--- PortfolioApp/routes/index.js
+++ PortfolioApp/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
-var router = express.Router();
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Welcome' });
 });
+
+router.get('/projects', (req, res) => {
+  res.render('projects', {title: "Projects"}) 
+});
+
+router.get('/about', (req, res) => {
+  res.render('about', { title: 'About' }) 
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact' }) 
+});
+
 
 module.exports = router;
